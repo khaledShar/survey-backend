da61be5127d4561c5e7818c536993bc37ab44b01
khaled Sharie <khaled.sharie@dacoso.com>
2025-09-14 16:49:14 +0200

    Initializes survey backend project

---
+++ src/main/java/com/khaled/survey_backend/SurveyBackendApplication.java
@@ -0,0 +1,13 @@
+package com.khaled.survey_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SurveyBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SurveyBackendApplication.class, args);
+	}
+
+}
